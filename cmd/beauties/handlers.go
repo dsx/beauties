27191fec90faaaea8f14e4f7f079ad593b5384e7
dsx <dsx@droidnest.org>
2017-05-01 10:47:21 -0400

    eppic fail foutrelis

---
+++ cmd/beauties/handlers.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func registerHandlers(r *mux.Router) {
+	r.HandleFunc("/", postHandler).Methods("POST")
+	r.HandleFunc("/", viewHandler).Methods("GET")
+	r.HandleFunc("/bash", bashHandler).Methods("GET")
+	r.HandleFunc("/f", formHandler).Methods("GET")
+	r.HandleFunc("/gpg.asc", gpgHandler).Methods("GET")
+	r.HandleFunc("/ip", ipHandler).Methods("GET")
+	r.HandleFunc("/{filename}", putHandler).Methods("PUT")
+	r.HandleFunc("/{token}/{filename}", deleteHandler).Methods("DELETE")
+	r.HandleFunc("/{token}/{filename}", getHandler).Methods("GET")
+	r.HandleFunc("/{token}/{filename}", headHandler).Methods("HEAD")
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func staticAssetHandler(w http.ResponseWriter, asset string) {
+	data, err := Asset(asset)
+	if err != nil {
+		log.Printf("Can't retrieve asset %s: %s", asset, err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(data); err != nil {
+		log.Printf("Can't write asset %s data: %s", asset, err.Error())
+		w.Header().Set("Content-Type", "text/html")
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	asset := "static/index.txt"
+	domain := getDomain(r)
+	data, err := Asset(asset)
+	if err != nil {
+		log.Printf("Can't retrieve asset %s: %s", asset, err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	tmpl, err := template.New("index.txt").Parse(string(data))
+	w.Header().Set("Content-Type", "text/plain")
+	tmpl.Execute(w, map[string]string{"domain": domain})
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	staticAssetHandler(w, "static/form.html")
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	remoteAddr := r.Header.Get("X-Real-IP")
+	if remoteAddr == "" {
+		remoteAddr = r.RemoteAddr
+		idx := strings.LastIndex(remoteAddr, ":")
+		if idx != -1 {
+			remoteAddr = remoteAddr[:idx]
+		}
+	}
+	fmt.Fprintf(w, "%s\n", remoteAddr)
+}
+
+func gpgHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	staticAssetHandler(w, "static/gpg.asc")
+}
+
+func bashHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	staticAssetHandler(w, "static/bash")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkFreeSpace() {
+		log.Printf("%s: not enough free space to store file", storage)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if err := r.ParseMultipartForm(RequestMaximumMemory); nil != err {
+		log.Printf("Can't parse form: %s", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	for _, fheaders := range r.MultipartForm.File {
+		for _, fheader := range fheaders {
+			filename := filepath.Base(strings.TrimSpace(fheader.Filename))
+			contentType := fheader.Header.Get("Content-Type")
+
+			if contentType == "" {
+				contentType = mime.TypeByExtension(filepath.Ext(filename))
+			}
+
+			fh, err := fheader.Open()
+			if err != nil {
+				log.Printf("Can't get filehandler for uploaded file %s: %s", filename, err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+
+			token, contentLength, err := genericUploadHandler(fh, filename, contentType)
+			if err != nil {
+				log.Printf("File upload failed for file %s: %s", filename, err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+
+			url := makeURL(r, token, filename)
+			log.Printf("Upload complete: %s (%d)", url, contentLength)
+			io.WriteString(w, url+"\n")
+		}
+	}
+}
+
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkFreeSpace() {
+		log.Printf("%s: not enough free space to store file", storage)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	vars := mux.Vars(r)
+	filename := filepath.Base(strings.TrimSpace(vars["filename"]))
+
+	contentType := r.Header.Get("Content-Type")
+
+	if contentType == "" {
+		contentType = mime.TypeByExtension(filepath.Ext(vars["filename"]))
+	}
+
+	fh := r.Body
+
+	token, contentLength, err := genericUploadHandler(fh, filename, contentType)
+	if err != nil {
+		log.Printf("File upload failed for file %s: %s", filename, err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	url := makeURL(r, token, filename)
+	log.Printf("Upload complete: %s (%d)", url, contentLength)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, url+"\n")
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	fileManipulationHandler("Get", w, r)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	fileManipulationHandler("Delete", w, r)
+}
+
+func headHandler(w http.ResponseWriter, r *http.Request) {
+	fileManipulationHandler("Head", w, r)
+}
+
+func genericUploadHandler(fh io.Reader, fn, contentType string) (token string, contentLength int64, err error) {
+	filename := filepath.Base(fn)
+	token = getToken()
+
+	if contentType == "" {
+		contentType = mime.TypeByExtension(filename)
+	}
+
+	var buff bytes.Buffer
+
+	contentLength, err = io.CopyN(&buff, fh, RequestMaximumMemory+1)
+	if err != nil && err != io.EOF {
+		log.Printf("Can't write to buffer: %s", err.Error())
+		return
+	}
+
+	var reader io.Reader
+	if contentLength > RequestMaximumMemory {
+		var file *os.File
+		file, err = ioutil.TempFile(config.Temp, fmt.Sprintf("%s-", BinaryName))
+
+		if err != nil {
+			log.Printf("Can't create temp file: %s", err.Error())
+			return
+		}
+		defer file.Close()
+
+		contentLength, err = io.Copy(file, io.MultiReader(&buff, fh))
+		if err != nil {
+			log.Printf("Can't write to temp file %s: %s", file.Name(), err.Error())
+			os.Remove(file.Name())
+			return
+		}
+
+		reader, err = os.Open(file.Name())
+		if err != nil {
+			log.Printf("Can't open temp file %s for reading: %s", file.Name(), err.Error())
+			os.Remove(file.Name())
+			return
+		}
+	} else {
+		reader = bytes.NewReader(buff.Bytes())
+	}
+
+	if err = storage.Put(token, filename, reader, contentType, contentLength); err != nil {
+		log.Printf("Can't write to storage %s: %s", storage, err.Error())
+	}
+
+	return
+}
+
+func fileManipulationHandler(op string, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	token := vars["token"]
+	filename := vars["filename"]
+	var (
+		contentLength int64
+		contentType   string
+		err           error
+		reader        io.ReadCloser
+	)
+
+	switch op {
+	case "Get":
+		reader, contentType, contentLength, err = storage.Get(token, filename)
+		if err == nil {
+			defer reader.Close()
+		}
+
+	case "Delete":
+		err = storage.Delete(token, filename)
+
+	case "Head":
+		contentType, contentLength, err = storage.Head(token, filename)
+	}
+
+	if err != nil {
+		if storage.IsNotExist(err) {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		log.Printf("Can't %s file %s/%s from storage %s: %s", op, token, filename, storage, err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+
+	}
+
+	switch op {
+	case "Get":
+		if _, err = io.Copy(w, reader); err != nil {
+			log.Printf("Can't read file %s/%s from storage %s: %s", token, filename, storage, err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		fallthrough
+
+	case "Head":
+		w.Header().Set("Content-Type", contentType)
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", contentLength))
+		w.Header().Set("Connection", "close")
+
+	case "Delete":
+		http.Error(w, http.StatusText(http.StatusNoContent), http.StatusNoContent)
+	}
+}
---
+++ cmd/beauties/main.go
@@ -0,0 +1,124 @@
+//go:generate go-bindata static
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"mime"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/dsx/beauties"
+	"github.com/gorilla/mux"
+)
+
+// BinaryName defines name of the binary for help message, default
+// storage dir name, prefix for temporary files, prefix for some log
+// messages and environment variable (converted to uppercase)
+var BinaryName = "beauties"
+
+// RequestMaximumMemory maximum size of request to be processed in memory
+var RequestMaximumMemory int64 = 1 * 1024 * 1024
+
+var config struct {
+	Bind       string
+	DomainName string
+	Log        string
+	MinSpace   float64
+	Storage    string
+	Temp       string
+}
+
+var storage beauties.Storage
+
+func init() {
+	// Bind
+	config.Bind = ":8080"
+	flag.StringVar(&config.Bind, "b", config.Bind, "IP:PORT to bind to")
+
+	// Log
+	flag.StringVar(&config.Log, "l", "", "log file (default \"stderr\")")
+
+	// MinSpace
+	flag.Float64Var(&config.MinSpace, "m", 0.2, "minimum free space 0…1")
+
+	// Storage
+	config.Storage = os.Getenv(fmt.Sprintf("%s_STORAGE", strings.ToUpper(BinaryName)))
+	if config.Storage == "" {
+		config.Storage = fmt.Sprintf("/var/cache/%s", BinaryName)
+	}
+	flag.StringVar(&config.Storage, "s", config.Storage, "storage directory")
+
+	// Temp
+	flag.StringVar(&config.Temp, "t", os.TempDir(), "temporary directory")
+
+	// DomainName
+	config.DomainName = os.Getenv(fmt.Sprintf("%s_DOMAIN", strings.ToUpper(BinaryName)))
+	flag.StringVar(&config.DomainName, "d", config.DomainName, "domain name to use in response (default is guess from request)")
+
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Printf("Usage: %s [-b 192.0.2.1:80] [-d example.com] [-l /var/log/%s] [-m 0.2] [-s /var/cache/%s] [-t /tmp]\n", BinaryName, BinaryName, BinaryName)
+	flag.PrintDefaults()
+	fmt.Println("")
+}
+
+func main() {
+	var err error
+	rand.Seed(time.Now().UTC().UnixNano())
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	r := mux.NewRouter()
+	registerHandlers(r)
+
+	if config.Log != "" {
+		f, err := os.OpenFile(config.Log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatalf("Can't open log file %s for writing: %s", config.Log, err.Error())
+		}
+
+		defer f.Close()
+
+		log.SetOutput(f)
+	}
+
+	storage, err = beauties.NewLocalStorage(config.Storage)
+	if err != nil {
+		log.Fatalf("Can't initialize storage %s: %s", config.Storage, err.Error())
+	}
+
+	mime.AddExtensionType(".md", "text/x-markdown")
+
+	log.Printf("%s server started: listening on %s, using temp directory %s, storing files to %s",
+		BinaryName, config.Bind, config.Temp, config.Storage)
+
+	s := &http.Server{
+		Addr:         config.Bind,
+		Handler:      logRequest(r),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		s.ListenAndServe()
+	}()
+
+	term := make(chan os.Signal, 1)
+	signal.Notify(term, os.Interrupt)
+	signal.Notify(term, syscall.SIGTERM)
+
+	<-term
+
+	log.Printf("%s server stopped.", BinaryName)
+}
